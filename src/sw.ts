afaab7d451562453512a8924483305abe65849f0
keifufu <keifufu@noonly.net>
2023-02-21 11:41:16 +0100

    improve action title

--- src/sw.ts
+++ src/sw.ts
@@ -1,14 +1,33 @@
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.event === 'outdated') {
-    chrome.action.setBadgeText({ text: '!' })
-    // Red, Green, Blue, Alpha
-    chrome.action.setBadgeBackgroundColor({ color: [255, 0, 0, 255] })
-    chrome.action.setTitle({ title: 'Connected but plugin is outdated. Click to update' })
-  } else if (request.event === 'wsDisconnected') {
-    chrome.action.setBadgeText({ text: '!' })
-    chrome.action.setTitle({ title: 'Not connected to Rainmeter. Click to troubleshoot' })
-  } else if (request.event === 'wsConnected') {
-    chrome.action.setBadgeText({ text: '' })
-    chrome.action.setTitle({ title: 'Connected and sending info' })
+const openTabs: { [key: string]: boolean } = {}
+
+const updateTitle = () => {
+  const tabs = Object.keys(openTabs).length
+  chrome.action.setTitle({ title: `${tabs} tab${tabs === 1 ? '' : 's'} connected` })
+}
+updateTitle()
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+  switch (request.event) {
+    case 'outdated':
+      chrome.action.setBadgeText({ text: '!' })
+      chrome.action.setBadgeBackgroundColor({ color: [255, 0, 0, 255] })
+      chrome.action.setTitle({ title: 'WebNowPlaying plugin is outdated' })
+      break
+    case 'wsConnected':
+      if (!sender.tab || !sender.tab.id) break
+      if (openTabs[sender.tab.id]) break
+      openTabs[sender.tab.id] = true
+      updateTitle()
+      chrome.action.setBadgeText({ text: '' }) // Reset it in case it was set to '!'
+      break
+    case 'wsDisconnected':
+      if (!sender.tab || !sender.tab.id) break
+      if (!openTabs[sender.tab.id]) break
+      delete openTabs[sender.tab.id]
+      updateTitle()
+      chrome.action.setBadgeText({ text: '' }) // Reset it in case it was set to '!'
+      break
+    default:
+      break
   }
 })
