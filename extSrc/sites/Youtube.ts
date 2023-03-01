84a18fbbd2082517f19fb37ad4e14927c9319b59
keifufu <github@keifufu.dev>
2023-03-01 23:50:14 +0100

    auto error reporting for youtube

--- extSrc/sites/Youtube.ts
+++ extSrc/sites/Youtube.ts
@@ -1,89 +1,77 @@
 import { RepeatMode, Site, StateMode } from '../content'
-import { timeInSecondsToString } from '../utils'
+import { querySelector, querySelectorEvent, querySelectorEventReport, querySelectorReport, timeInSecondsToString } from '../utils'
+
+// TODO: support the tiny player it sometimes gives you in the bottom right corner
 
 // I'm not using mediaSession here because of ads.
 // Of course this isn't an issue with YouTube Premium or adblock, but still.
 const site: Site = {
-  ready: () => (document.querySelector<HTMLElement>('.ytd-video-primary-info-renderer.title')?.innerText.length || 0) > 0,
+  ready: () => querySelector<boolean, HTMLElement>('.ytd-video-primary-info-renderer.title', (el) => el.innerText.length > 0, false),
   info: {
-    player: () => 'Youtube',
+    player: () => 'YouTube',
     state: () => {
-      let state = document.querySelector<HTMLVideoElement>('.html5-main-video')?.paused ? StateMode.PAUSED : StateMode.PLAYING
+      let state = querySelectorReport<StateMode, HTMLVideoElement>('.html5-main-video', (el) => (el.paused ? StateMode.PAUSED : StateMode.PLAYING), StateMode.PAUSED, 'state')
       // It is possible for the video to be "playing" but not started
-      if (state === StateMode.PLAYING && (document.querySelector<HTMLVideoElement>('.html5-main-video')?.played.length || 0) <= 0)
+      if (state === StateMode.PLAYING && querySelector<boolean, HTMLVideoElement>('.html5-main-video', (el) => el.played.length <= 0, false))
         state = StateMode.PAUSED
       return state
     },
-    title: () => document.querySelector<HTMLElement>('.ytd-video-primary-info-renderer.title')?.innerText || '',
-    artist: () => document.querySelector<HTMLElement>('.ytd-video-secondary-info-renderer .ytd-channel-name a')?.innerText || '',
-    album: () => {
-      const playlistTitle = document.querySelector<HTMLElement>('#header-description a')
-      if (playlistTitle !== null) return playlistTitle.innerText
-
-      return ''
-    },
+    title: () => querySelectorReport<string, HTMLElement>('.ytd-video-primary-info-renderer.title', (el) => el.innerText, '', 'title'),
+    artist: () => querySelectorReport<string, HTMLElement>('.ytd-video-secondary-info-renderer .ytd-channel-name a', (el) => el.innerText, '', 'artist'),
+    album: () => querySelector<string, HTMLElement>('#header-description a', (el) => el.innerText, ''),
     cover: () => {
       const videoId = new URLSearchParams(window.location.search).get('v')
       return `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`
     },
-    duration: () => document.querySelector<HTMLElement>('.ytp-time-duration')?.innerText || '0:00',
-    position: () => timeInSecondsToString(document.querySelector<HTMLVideoElement>('.html5-main-video')?.currentTime || 0),
-    volume: () => {
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (!video || video.muted) return 0
-      return (video.volume * 100)
-    },
+    duration: () => querySelectorReport<string, HTMLVideoElement>('.html5-main-video', (el) => timeInSecondsToString(el.duration), '0:00', 'duration'),
+    position: () => querySelectorReport<string, HTMLVideoElement>('.html5-main-video', (el) => timeInSecondsToString(el.currentTime), '0:00', 'position'),
+    volume: () => querySelectorReport<number, HTMLVideoElement>('.html5-main-video', (el) => (el.muted ? 0 : el.volume * 100), 100, 'volume'),
     rating: () => {
-      const likeButtonPressed = document.querySelector('#segmented-like-button button')?.getAttribute('aria-pressed')
-      const dislikeButtonPressed = document.querySelector('#segmented-dislike-button button')?.getAttribute('aria-pressed')
-      if (likeButtonPressed === 'true') return 5
-      if (dislikeButtonPressed === 'true') return 1
+      const likeButtonPressed = querySelectorReport<boolean, HTMLButtonElement>('#segmented-like-button button', (el) => el.getAttribute('aria-pressed') === 'true', false, 'rating')
+      if (likeButtonPressed) return 5
+      const dislikeButtonPressed = querySelectorReport<boolean, HTMLButtonElement>('#segmented-dislike-button button', (el) => el.getAttribute('aria-pressed') === 'true', false, 'rating')
+      if (dislikeButtonPressed) return 1
       return 0
     },
     repeat: () => {
-      const playlistRepeatButtonSvgPath = document.querySelector('#playlist-action-menu path')?.getAttribute('d')
-      const svgPathLoopPlaylist = 'M20,14h2v5L5.84,19.02l1.77,1.77l-1.41,1.41L1.99,18l4.21-4.21l1.41,1.41l-1.82,1.82L20,17V14z M4,7l14.21-0.02l-1.82,1.82 l1.41,1.41L22.01,6l-4.21-4.21l-1.41,1.41l1.77,1.77L2,5v6h2V7z'
-
       // If the playlist loop is set to video, it sets the video to loop
-      if (document.querySelector<HTMLVideoElement>('.html5-main-video')?.loop) return RepeatMode.ONE
+      if (querySelector<boolean, HTMLVideoElement>('.html5-main-video', (el) => el.loop, false)) return RepeatMode.ONE
+      const playlistRepeatButtonSvgPath = querySelector<string, HTMLElement>('#playlist-action-menu path', (el) => el.getAttribute('d'), '')
+      const svgPathLoopPlaylist = 'M20,14h2v5L5.84,19.02l1.77,1.77l-1.41,1.41L1.99,18l4.21-4.21l1.41,1.41l-1.82,1.82L20,17V14z M4,7l14.21-0.02l-1.82,1.82 l1.41,1.41L22.01,6l-4.21-4.21l-1.41,1.41l1.77,1.77L2,5v6h2V7z'
       if (playlistRepeatButtonSvgPath === svgPathLoopPlaylist) return RepeatMode.ALL
       return RepeatMode.NONE
     },
-    shuffle: () => {
-      // eslint-disable-next-line prefer-destructuring
-      const shuffleButtonInPlaylists = document.querySelectorAll('#playlist-action-menu button')[1]
-      return shuffleButtonInPlaylists?.getAttribute('aria-pressed') === 'true'
-    }
+    shuffle: () => querySelector<boolean, HTMLButtonElement>('(#playlist-action-menu button)[1]', (el) => el.getAttribute('aria-pressed') === 'true', false)
   },
   events: {
-    togglePlaying: () => document.querySelector<HTMLButtonElement>('.ytp-play-button')?.click(),
-    next: () => document.querySelector<HTMLButtonElement>('.ytp-next-button')?.click(),
-    previous: () => document.querySelector<HTMLButtonElement>('.ytp-prev-button')?.click(),
-    setPositionSeconds: (positionInSeconds: number) => {
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (video) video.currentTime = positionInSeconds
+    togglePlaying: () => querySelectorEventReport<HTMLButtonElement>('.ytp-play-button', (el) => el.click(), 'togglePlaying'),
+    next: () => querySelectorEventReport<HTMLButtonElement>('.ytp-next-button', (el) => el.click(), 'next'),
+    previous: () => {
+      querySelectorEventReport<HTMLVideoElement>('.html5-main-video', (el) => {
+        if (el.currentTime > 5) el.currentTime = 0
+        else querySelectorEventReport<HTMLButtonElement>('.ytp-prev-button', (el) => el.click(), 'previous')
+      }, 'previous')
     },
+    setPositionSeconds: (positionInSeconds: number) => querySelectorEventReport<HTMLVideoElement>('.html5-main-video', (el) => el.currentTime = positionInSeconds, 'setPositionSeconds'),
     setPositionPercentage: null,
     setVolume: (volume: number) => {
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (video) {
-        video.volume = volume / 100
-        if (volume === 0) video.muted = true
-        else video.muted = false
-      }
+      querySelectorEventReport<HTMLVideoElement>('.html5-main-video', (el) => {
+        el.volume = volume / 100
+        if (volume === 0) el.muted = true
+        else el.muted = false
+      }, 'setVolume')
     },
     toggleRepeat: () => {
       // If no playlist repeat button exists, set the video to loop, otherwise click the playlist loop button
-      const playlistLoopButton = document.querySelector<HTMLButtonElement>('#playlist-action-menu button')
-      if (playlistLoopButton !== null)
-        return playlistLoopButton.click()
-
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (video) video.loop = !video.loop
+      // The playlist buttons remain after switching to a regular video, so we check if there is a playlist in the URI
+      const list = new URLSearchParams(window.location.search).get('list')
+      let success = false
+      if (list) success = querySelectorEvent<HTMLButtonElement>('#playlist-action-menu button', (el) => el.click())
+      if (!success) querySelectorEventReport<HTMLVideoElement>('.html5-main-video', (el) => el.loop = !el.loop, 'toggleRepeat')
     },
-    toggleShuffle: () => document.querySelectorAll<HTMLButtonElement>('#playlist-action-menu button')[1]?.click(),
-    toggleThumbsUp: () => document.querySelector<HTMLButtonElement>('#segmented-like-button button')?.click(),
-    toggleThumbsDown: () => document.querySelector<HTMLButtonElement>('#segmented-dislike-button button')?.click(),
+    toggleShuffle: () => querySelectorEventReport<HTMLButtonElement>('(#playlist-action-menu button)[1]', (el) => el.click(), 'toggleShuffle'),
+    toggleThumbsUp: () => querySelectorEventReport<HTMLButtonElement>('#segmented-like-button button', (el) => el.click(), 'toggleThumbsUp'),
+    toggleThumbsDown: () => querySelectorEventReport<HTMLButtonElement>('#segmented-dislike-button button', (el) => el.click(), 'toggleThumbsDown'),
     setRating: (rating: number) => {
       if (rating >= 3 && site.info.rating?.() !== 5)
         site.events.toggleThumbsUp?.()
