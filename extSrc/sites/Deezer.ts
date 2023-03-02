2c85b2b23b0b2278268c0450ec06a6dbcd3f2fe3
keifufu <github@keifufu.dev>
2023-03-02 02:42:36 +0100

    auto report for deezer

--- extSrc/sites/Deezer.ts
+++ extSrc/sites/Deezer.ts
@@ -1,58 +1,51 @@
 import { RepeatMode, Site, StateMode } from '../content'
-import { getMediaSessionCover } from '../utils'
+import { getMediaSessionCover, querySelector, querySelectorEventReport, querySelectorReport } from '../utils'
 
 const site: Site = {
-  ready: () => navigator.mediaSession.metadata !== null && document.querySelector('.track-link') !== null,
+  ready: () => navigator.mediaSession.metadata !== null && querySelector<boolean, HTMLElement>('.track-link', (el) => el !== null, false),
   info: {
     player: () => 'Deezer',
-    state: () => (document.querySelectorAll('.player-controls svg')[1].getAttribute('data-testid') === 'PauseIcon' ? StateMode.PLAYING : StateMode.PAUSED),
+    state: () => querySelectorReport<StateMode, HTMLElement>('(.player-controls svg)[1]', (el) => (el.getAttribute('data-testid') === 'PauseIcon' ? StateMode.PLAYING : StateMode.PAUSED), StateMode.PAUSED, 'state'),
     title: () => navigator.mediaSession.metadata?.title || '',
     artist: () => navigator.mediaSession.metadata?.artist || '',
     album: () => navigator.mediaSession.metadata?.album || '',
     cover: () => getMediaSessionCover(),
-    duration: () => document.querySelector<HTMLElement>('.slider-counter-max')?.innerText || '0:00',
-    position: () => document.querySelector<HTMLElement>('.slider-counter-current')?.innerText || '0:00',
-    volume: () => 100, // TODO: Not supported for now
-    rating: () => (document.querySelectorAll('.track-actions svg')[2].getAttribute('data-testid') === 'HeartIcon' ? 0 : 5),
-    repeat: () => {
-      const svg = document.querySelectorAll('.option-item svg')[1]
-      if (!svg) return RepeatMode.NONE
-      const isActive = getComputedStyle(svg).color === 'rgb(239, 84, 102)'
-      if (svg.getAttribute('data-testid') === 'RepeatIcon' && isActive) return RepeatMode.ALL
-      if (svg.getAttribute('data-testid') === 'RepeatOneIcon') return RepeatMode.ONE
+    duration: () => querySelectorReport<string, HTMLElement>('.slider-counter-max', (el) => el.innerText, '0:00', 'duration'),
+    position: () => querySelectorReport<string, HTMLElement>('.slider-counter-current', (el) => el.innerText, '0:00', 'position'),
+    volume: () => 100,
+    rating: () => querySelectorReport<number, HTMLElement>('(.track-actions svg)[2]', (el) => (el.getAttribute('data-testid') === 'HeartIcon' ? 0 : 5), 0, 'rating'),
+    repeat: () => querySelectorReport<RepeatMode, HTMLElement>('.option-item', (el) => {
+      const isActive = getComputedStyle(el).color === 'rgb(239, 84, 102)'
+      if (el.getAttribute('data-testid') === 'RepeatIcon' && isActive) return RepeatMode.ALL
+      if (el.getAttribute('data-testid') === 'RepeatOneIcon') return RepeatMode.ONE
       return RepeatMode.NONE
-    },
-    shuffle: () => {
-      const svg = document.querySelectorAll('.option-item svg')[2]
-      if (!svg) return false
-      const isActive = getComputedStyle(svg).color === 'rgb(239, 84, 102)'
-      return isActive
-    }
+    }, RepeatMode.NONE, 'repeat'),
+    shuffle: () => querySelectorReport<boolean, HTMLElement>('(.option-item svg)[2]', (el) => getComputedStyle(el).color === 'rgb(239, 84, 102)', false, 'shuffle')
   },
   events: {
-    togglePlaying: () => document.querySelectorAll<HTMLButtonElement>('.player-controls button')[1]?.click(),
-    next: () => () => document.querySelectorAll<HTMLButtonElement>('.player-controls button')[2]?.click(),
-    previous: () => () => document.querySelectorAll<HTMLButtonElement>('.player-controls button')[0]?.click(),
+    togglePlaying: () => querySelectorEventReport<HTMLButtonElement>('(.player-controls button)[1]', (el) => el.click(), 'togglePlaying'),
+    next: () => querySelectorEventReport<HTMLButtonElement>('(.player-controls button)[2]', (el) => el.click(), 'next'),
+    previous: () => querySelectorEventReport<HTMLButtonElement>('(.player-controls button)[0]', (el) => el.click(), 'previous'),
     setPositionSeconds: null,
     setPositionPercentage: (positionPercentage: number) => {
-      const el = document.querySelector<HTMLInputElement>('.slider-track-input')
-      if (!el) return
-      const loc = el.getBoundingClientRect()
-      const position = positionPercentage * loc.width
+      querySelectorEventReport<HTMLInputElement>('.slider-track-input', (el) => {
+        const loc = el.getBoundingClientRect()
+        const position = positionPercentage * loc.width
 
-      el.value = (positionPercentage * parseFloat(el.getAttribute('aria-valuemax') || '0')).toString()
-      el.dispatchEvent(new MouseEvent('mouseup', {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-        clientX: loc.left + position,
-        clientY: loc.top + (loc.height / 2)
-      }))
+        el.value = (positionPercentage * parseFloat(el.getAttribute('aria-valuemax') || '0')).toString()
+        el.dispatchEvent(new MouseEvent('mouseup', {
+          view: window,
+          bubbles: true,
+          cancelable: true,
+          clientX: loc.left + position,
+          clientY: loc.top + (loc.height / 2)
+        }))
+      }, 'setPositionPercentage')
     },
-    setVolume: null, // TODO: Not supported for now
-    toggleRepeat: () => document.querySelectorAll<HTMLButtonElement>('.option-item button')[1]?.click(),
-    toggleShuffle: () => document.querySelectorAll<HTMLButtonElement>('.option-item button')[2]?.click(),
-    toggleThumbsUp: () => document.querySelectorAll<HTMLButtonElement>('.track-actions button')[2]?.click(),
+    setVolume: null,
+    toggleRepeat: () => querySelectorEventReport<HTMLButtonElement>('(.option-item button)[1]', (el) => el.click(), 'toggleRepeat'),
+    toggleShuffle: () => querySelectorEventReport<HTMLButtonElement>('(.option-item button)[2]', (el) => el.click(), 'toggleShuffle'),
+    toggleThumbsUp: () => querySelectorEventReport<HTMLButtonElement>('(.track-actions button)[2]', (el) => el.click(), 'toggleThumbsUp'),
     toggleThumbsDown: null,
     setRating: (rating: number) => {
       if (rating >= 3 && site.info.rating?.() !== 5)
