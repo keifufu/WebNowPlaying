c0298ec49273ffc002731102562c967da50ad5c0
keifufu <github@keifufu.dev>
2023-03-02 19:30:24 +0100

    auto report for youtube embed

--- extSrc/sites/YoutubeEmbed.ts
+++ extSrc/sites/YoutubeEmbed.ts
@@ -1,52 +1,42 @@
 import { RepeatMode, Site, StateMode } from '../content'
-import { timeInSecondsToString } from '../utils'
+import { querySelector, querySelectorEventReport, querySelectorReport, timeInSecondsToString } from '../utils'
 
 let shuffleState = false
 let playlistLoaded = false
 
 const site: Site = {
   ready: () => {
+    // TODO:
     const title = document.querySelector<HTMLElement>('.ytp-title-text')
     return title !== null && title.innerText?.length > 0 && !document.querySelector('.html5-video-player')?.classList.contains('unstarted-mode')
   },
   info: {
     player: () => 'Youtube Embed',
     state: () => {
-      let state = document.querySelector<HTMLVideoElement>('.html5-main-video')?.paused ? StateMode.PAUSED : StateMode.PLAYING
+      let state = querySelectorReport<StateMode, HTMLVideoElement>('.html5-main-video', (el) => (el.paused ? StateMode.PAUSED : StateMode.PLAYING), StateMode.PAUSED, 'state')
       // It is possible for the video to be "playing" but not started
-      if (state === StateMode.PLAYING && (document.querySelector<HTMLVideoElement>('.html5-main-video')?.played.length || 0) <= 0)
+      if (state === StateMode.PLAYING && querySelector<boolean, HTMLVideoElement>('.html5-main-video', (el) => el.played.length <= 0, false))
         state = StateMode.PAUSED
       return state
     },
-    title: () => document.querySelector<HTMLElement>('.ytp-title-text')?.innerText || '', // The title of the current song
-    artist: () => document.querySelector<HTMLElement>('.ytp-title-expanded-title')?.innerText || '', // The artist of the current song
-    album: () => {
-      const playlist = document.querySelector<HTMLElement>('.ytp-playlist-menu-title')
-      if (playlist && playlist.innerText.length > 0)
-        return playlist.innerText
-      return ''
-    },
+    title: () => querySelectorReport<string, HTMLElement>('.ytp-title-text', (el) => el.innerText, '', 'title'),
+    artist: () => querySelectorReport<string, HTMLElement>('.ytp-title-expanded-title', (el) => el.innerText, '', 'artist'),
+    album: () => querySelector<string, HTMLElement>('.ytp-playlist-menu-title', (el) => el.innerText, ''),
     cover: () => {
       const videoId = new URLSearchParams(document.querySelector<HTMLAnchorElement>('.ytp-title-link')?.search).get('v')
       return `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`
     },
-    duration: () => (
-      document.querySelector<HTMLElement>('.ytp-time-duration')?.innerText ||
-        timeInSecondsToString(document.querySelector<HTMLVideoElement>('.html5-main-video')?.duration || 0)
-    ),
-    position: () => timeInSecondsToString(document.querySelector<HTMLVideoElement>('.html5-main-video')?.currentTime || 0),
-    volume: () => {
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (!video || video.muted) return 0
-      return (video.volume * 100)
-    },
+    duration: () => querySelectorReport<string, HTMLVideoElement>('.html5-main-video', (el) => timeInSecondsToString(el.duration), '0:00', 'duration'),
+    position: () => querySelectorReport<string, HTMLVideoElement>('.html5-main-video', (el) => timeInSecondsToString(el.currentTime), '0:00', 'position'),
+    volume: () => querySelectorReport<number, HTMLVideoElement>('.html5-main-video', (el) => (el.muted ? 0 : el.volume * 100), 0, 'volume'),
     rating: () => 0,
     repeat: () => RepeatMode.NONE,
     shuffle: () => shuffleState
   },
   events: {
-    togglePlaying: () => document.querySelector<HTMLButtonElement>('.ytp-play-button')?.click(),
+    togglePlaying: () => querySelectorEventReport<HTMLButtonElement>('.ytp-play-button', (el) => el.click(), 'togglePlaying'),
     next: () => {
+      // Not using reporting querySelectors right now
       const list = new URLSearchParams(document.querySelector<HTMLAnchorElement>('.ytp-title-link')?.search).get('list')
       if (shuffleState && list) {
         const playlist = document.querySelector('.ytp-playlist-menu-items')
@@ -66,6 +56,7 @@ const site: Site = {
       }
     },
     previous: () => {
+      // Not using reporting querySelectors right now
       const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
       if (!video) return
       const previousButton = document.querySelector<HTMLButtonElement>('.ytp-prev-button')
@@ -92,24 +83,18 @@ const site: Site = {
         video.currentTime = 0
       }
     },
-    setPositionSeconds: (positionInSeconds: number) => {
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (video) video.currentTime = positionInSeconds
-    },
+    setPositionSeconds: (positionInSeconds: number) => querySelectorEventReport<HTMLVideoElement>('.html5-main-video', (el) => el.currentTime = positionInSeconds, 'setPositionSeconds'),
     setPositionPercentage: null,
     setVolume: (volume: number) => {
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (video) {
-        video.volume = volume / 100
-        if (volume === 0) video.muted = true
-        else video.muted = false
-      }
+      querySelectorEventReport<HTMLVideoElement>('.html5-main-video', (el) => {
+        el.volume = volume / 100
+        if (volume === 0) el.muted = true
+        else el.muted = false
+      }, 'setVolume')
     },
-    toggleRepeat: () => {
-      const video = document.querySelector<HTMLVideoElement>('.html5-main-video')
-      if (video) video.loop = !video.loop
-    },
+    toggleRepeat: () => querySelectorEventReport<HTMLVideoElement>('.html5-main-video', (el) => el.loop = !el.loop, 'toggleRepeat'),
     toggleShuffle: () => {
+      // Not using reporting querySelectors right now
       const list = new URLSearchParams(document.querySelector<HTMLAnchorElement>('.ytp-title-link')?.search).get('list')
       if (list) shuffleState = !shuffleState
       else shuffleState = false
