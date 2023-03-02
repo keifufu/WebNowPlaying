07e2d8d8a9f32b490cfa1ba2f7a73a842ca7f917
keifufu <keifufu@noonly.net>
2023-03-02 01:58:43 +0100

    fix bandcamp and add auto-report

--- extSrc/sites/Bandcamp.ts
+++ extSrc/sites/Bandcamp.ts
@@ -1,47 +1,49 @@
 import { RepeatMode, Site, StateMode } from '../content'
-import { capitalize, timeInSecondsToString } from '../utils'
+import { capitalize, querySelector, querySelectorEventReport, querySelectorReport, timeInSecondsToString } from '../utils'
 
 const site: Site = {
-  ready: () => document.querySelector('audio') !== null,
+  ready: () => querySelector<boolean, HTMLAudioElement>('audio', (el) => el !== null, false),
   info: {
     player: () => 'Bandcamp',
-    state: () => (document.querySelector('audio')?.paused ? StateMode.PAUSED : StateMode.PLAYING),
-    title: () => document.querySelector<HTMLElement>('.trackTitle')?.innerText || document.querySelector<HTMLElement>('.title')?.innerText || '',
-    artist: () => document.querySelector<HTMLElement>('.artist span')?.innerText || capitalize(document.location.host.split('.')[0]),
-    album: () => document.querySelector<HTMLElement>('.fromAlbum')?.innerText || '',
-    cover: () => document.querySelector<HTMLImageElement>('.carousel-player-inner img')?.src || document.querySelector<HTMLImageElement>('.popupImage img')?.src || '',
-    duration: () => timeInSecondsToString(document.querySelector('audio')?.duration || 0),
-    position: () => timeInSecondsToString(document.querySelector('audio')?.currentTime || 0),
-    volume: () => (document.querySelector('audio')?.volume || 0) * 100,
+    state: () => querySelectorReport<StateMode, HTMLAudioElement>('audio', (el) => (el.paused ? StateMode.PAUSED : StateMode.PLAYING), StateMode.PAUSED, 'state'),
+    title: () => {
+      // TItle of the current song when playing in collections page
+      const collectionTitle = querySelector<string, HTMLElement>('[data-bind="text: currentTrack().trackTitle"]', (el) => el.innerText, '')
+      if (collectionTitle.length > 0) return collectionTitle
+      // Title of the current song when playing an album
+      const albumTitle = querySelector<string, HTMLElement>('.title', (el) => el.innerText, '')
+      if (albumTitle.length > 0) return albumTitle
+      return querySelector<string, HTMLElement>('.trackTitle', (el) => el.innerText, '')
+    },
+    artist: () => querySelectorReport<string, HTMLElement>('.artist span', (el) => el.innerText, capitalize(document.location.host.split('.')[0]), 'artist'),
+    album: () => {
+      // If .queue-icon is a element, we are listening to a album in a collection
+      if (document.querySelector('.queue-icon') !== null) return querySelector<string, HTMLElement>('.title', (el) => el.innerText, '')
+      // If .title is set, we are listening to an album
+      const albumTitle = querySelector<string, HTMLElement>('.title', (el) => el.innerText, '')
+      if (albumTitle.length > 0) return querySelector<string, HTMLElement>('.trackTitle', (el) => el.innerText, '')
+      return ''
+    },
+    cover: () => querySelectorReport<string, HTMLImageElement>('.carousel-player-inner img, .popupImage img', (el) => el.src, '', 'cover'),
+    duration: () => querySelectorReport<string, HTMLAudioElement>('audio', (el) => timeInSecondsToString(el.duration), '0:00', 'duration'),
+    position: () => querySelectorReport<string, HTMLAudioElement>('audio', (el) => timeInSecondsToString(el.currentTime), '0:00', 'position'),
+    volume: () => querySelectorReport<number, HTMLAudioElement>('audio', (el) => el.volume * 100, 100, 'volume'),
     rating: () => 0,
     repeat: () => RepeatMode.NONE,
     shuffle: () => false
   },
   events: {
-    togglePlaying: () => {
-      const button = document.querySelector<HTMLButtonElement>('.playpause') || document.querySelector<HTMLButtonElement>('.playbutton')
-      button?.click()
-    },
-    next: () => {
-      const button = document.querySelector<HTMLButtonElement>('.nextbutton') || document.querySelector<HTMLButtonElement>('.next')
-      button?.click()
-    },
-    previous: () => {
-      const button = document.querySelector<HTMLButtonElement>('.prevbutton') || document.querySelector<HTMLButtonElement>('.prev')
-      button?.click()
-    },
-    setPositionSeconds: (positionInSeconds: number) => {
-      const audio = document.querySelector('audio')
-      if (audio) audio.currentTime = positionInSeconds
-    },
+    togglePlaying: () => querySelectorEventReport<HTMLButtonElement>('.playpause, .playbutton', (el) => el.click(), 'togglePlaying'),
+    next: () => querySelectorEventReport<HTMLButtonElement>('.nextbutton, .next div', (el) => el.click(), 'next'),
+    previous: () => querySelectorEventReport<HTMLButtonElement>('.prevbutton, .prev div', (el) => el.click(), 'previous'),
+    setPositionSeconds: (positionInSeconds: number) => querySelectorEventReport<HTMLAudioElement>('audio', (el) => el.currentTime = positionInSeconds, 'setPositionSeconds'),
     setPositionPercentage: null,
     setVolume: (volume: number) => {
-      const audio = document.querySelector('audio')
-      if (audio) {
-        audio.volume = volume / 100
-        if (volume === 0) audio.muted = true
-        else audio.muted = false
-      }
+      querySelectorEventReport<HTMLAudioElement>('audio', (el) => {
+        el.volume = volume / 100
+        if (volume === 0) el.muted = true
+        else el.muted = false
+      }, 'setVolume')
     },
     toggleRepeat: null,
     toggleShuffle: null,
