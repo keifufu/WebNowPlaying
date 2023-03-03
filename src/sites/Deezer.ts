bdeb7c99b1547b160f8dec05538ac1f21eacb06c
keifufu <github@keifufu.dev>
2023-03-03 20:58:16 +0100

    Update Deezer

--- src/sites/Deezer.ts
+++ src/sites/Deezer.ts
@@ -1,8 +1,11 @@
 import { RepeatMode, Site, StateMode } from '../content'
 import { getMediaSessionCover, querySelector, querySelectorEventReport, querySelectorReport } from '../utils'
 
+// Not reporting duration, position and rating as it seems they disappear once deezer annoys you with its ads
+
 const site: Site = {
-  ready: () => navigator.mediaSession.metadata !== null && querySelector<boolean, HTMLElement>('.track-link', (el) => true, false),
+  ready: () =>
+    navigator.mediaSession.metadata !== null && querySelector<boolean, HTMLElement>('.track-link', (el) => true, false),
   info: {
     player: () => 'Deezer',
     state: () => querySelectorReport<StateMode, HTMLElement>('(.player-controls svg)[1]', (el) => (el.getAttribute('data-testid') === 'PauseIcon' ? StateMode.PLAYING : StateMode.PAUSED), StateMode.PAUSED, 'state'),
@@ -10,10 +13,10 @@ const site: Site = {
     artist: () => navigator.mediaSession.metadata?.artist || '',
     album: () => navigator.mediaSession.metadata?.album || '',
     cover: () => getMediaSessionCover(),
-    duration: () => querySelectorReport<string, HTMLElement>('.slider-counter-max', (el) => el.innerText, '0:00', 'duration'),
-    position: () => querySelectorReport<string, HTMLElement>('.slider-counter-current', (el) => el.innerText, '0:00', 'position'),
+    duration: () => querySelector<string, HTMLElement>('.slider-counter-max', (el) => el.innerText, '0:00'),
+    position: () => querySelector<string, HTMLElement>('.slider-counter-current', (el) => el.innerText, '0:00'),
     volume: () => 100,
-    rating: () => querySelectorReport<number, HTMLElement>('(.track-actions svg)[2]', (el) => (el.getAttribute('data-testid') === 'HeartIcon' ? 0 : 5), 0, 'rating'),
+    rating: () => querySelector<number, HTMLElement>('(.track-actions svg)[2]', (el) => (el.getAttribute('data-testid') === 'HeartIcon' ? 0 : 5), 0),
     repeat: () => querySelectorReport<RepeatMode, HTMLElement>('(.option-item svg)[1]', (el) => {
       const isActive = getComputedStyle(el).color === 'rgb(239, 84, 102)'
       if (el.getAttribute('data-testid') === 'RepeatIcon' && isActive) return RepeatMode.ALL
