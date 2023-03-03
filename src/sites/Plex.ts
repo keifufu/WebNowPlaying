0675298c03951dcc35e9eb727d5a74bab308c887
keifufu <github@keifufu.dev>
2023-03-03 19:45:31 +0100

    fix plex

--- src/sites/Plex.ts
+++ src/sites/Plex.ts
@@ -1,50 +1,50 @@
 import { RepeatMode, Site, StateMode } from '../content'
-import { getMediaSessionCover, querySelector, querySelectorEventReport, querySelectorReport, timeInSecondsToString } from '../utils'
+import { getMediaSessionCover, querySelector, querySelectorEvent, querySelectorEventReport, querySelectorReport, timeInSecondsToString } from '../utils'
 
 const site: Site = {
   ready: () => querySelector<boolean, HTMLVideoElement>('video', (el) => el !== null && el.duration > 0, false),
   info: {
     player: () => 'Plex',
     state: () => querySelectorReport<StateMode, HTMLVideoElement>('video', (el) => (el.paused ? StateMode.PAUSED : StateMode.PLAYING), StateMode.STOPPED, 'state'),
-    title: () => querySelectorReport<string, HTMLElement>('[class*="MetadataPosterTitle-title"]', (el) => el.innerText, '', 'title'),
-    artist: () => querySelectorReport<string, HTMLElement>('[data-testid="metadataYear"]', (el) => el.innerText, '', 'artist'),
+    // Not reporting because some views on Plex don't have a title (live TV)
+    title: () => querySelector<string, HTMLElement>('[class*="MetadataPosterTitle-title"]', (el) => el.innerText, document.title),
+    // Not reporting because some views on Plex don't have a artist (live TV)
+    artist: () => querySelector<string, HTMLElement>('[data-testid="metadataYear"]', (el) => el.innerText, ''),
     album: () => '',
     cover: () => getMediaSessionCover(),
     duration: () => querySelectorReport<string, HTMLVideoElement>('video', (el) => timeInSecondsToString(el.duration), '0:00', 'duration'),
     position: () => querySelectorReport<string, HTMLVideoElement>('video', (el) => timeInSecondsToString(el.currentTime), '0:00', 'position'),
     volume: () => querySelectorReport<number, HTMLVideoElement>('video', (el) => (el.muted ? 0 : el.volume * 100), 100, 'volume'),
     rating: () => 0,
-    repeat: () => {
-      const repeatButton = document.querySelector('button[data-testid="repeatButton"]')
-      if (repeatButton) {
-        if (repeatButton.className.includes('Active')) return RepeatMode.ALL
-      } else {
-        const repeatOneButton = document.querySelector('button[data-testid="repeatOneButton"]')
-        if (repeatOneButton) return RepeatMode.ONE
+    // Not reporting because some views on Plex don't have a repeat button (live TV)
+    repeat: () => querySelector<RepeatMode, HTMLButtonElement>('button[data-testid="repeatButton"], button[data-testid="repeatOneButton"]', (el) => {
+      if (el.className.includes('Active')) {
+        if (el.getAttribute('data-testid') === 'repeatButton') return RepeatMode.ALL
+        else return RepeatMode.ONE
       }
       return RepeatMode.NONE
-    },
-    shuffle: () => querySelectorReport<boolean, HTMLButtonElement>('button[data-testid="shuffleButton"]', (el) => el.className.includes('Active'), false, 'shuffle')
+    }, RepeatMode.NONE),
+    // Not reporting because some views on Plex don't have a shuffle button (live TV)
+    shuffle: () => querySelector<boolean, HTMLButtonElement>('button[data-testid="shuffleButton"]', (el) => el.className.includes('Active'), false)
   },
   events: {
-    // TODO: all of these
     togglePlaying: () => {
-      const button = document.querySelector('button[data-testid="pauseButton"], button[data-testid="resumeButton"]')
-      if (!button) return
-      button.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
-      button.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      querySelectorEventReport<HTMLButtonElement>('button[data-testid="pauseButton"], button[data-testid="resumeButton"], button[data-testid="closeButton"]', (el) => {
+        el.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+        el.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      }, 'togglePlaying')
     },
     next: () => {
-      const button = document.querySelector('button[data-testid="nextButton"]')
-      if (!button) return
-      button.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
-      button.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      querySelectorEventReport<HTMLButtonElement>('button[data-testid="nextButton"]', (el) => {
+        el.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+        el.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      }, 'next')
     },
     previous: () => {
-      const button = document.querySelector('button[data-testid="previousButton"]')
-      if (!button) return
-      button.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
-      button.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      querySelectorEventReport<HTMLButtonElement>('button[data-testid="previousButton"]', (el) => {
+        el.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+        el.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      }, 'previous')
     },
     setPositionSeconds: (positionInSeconds: number) => querySelectorEventReport<HTMLVideoElement>('video', (el) => el.currentTime = positionInSeconds, 'setPositionSeconds'),
     setPositionPercentage: null,
@@ -56,16 +56,16 @@ const site: Site = {
       }, 'setVolume')
     },
     toggleRepeat: () => {
-      const button = document.querySelector('button[data-testid="repeatButton"], button[data-testid="repeatOneButton"]')
-      if (!button) return
-      button.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
-      button.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      querySelectorEvent<HTMLButtonElement>('button[data-testid="repeatButton"], button[data-testid="repeatOneButton"]', (el) => {
+        el.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+        el.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      })
     },
     toggleShuffle: () => {
-      const button = document.querySelector('button[data-testid="shuffleButton"]')
-      if (!button) return
-      button.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
-      button.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      querySelectorEvent<HTMLButtonElement>('button[data-testid="shuffleButton"]', (el) => {
+        el.dispatchEvent(new MouseEvent('mousedown', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+        el.dispatchEvent(new MouseEvent('mouseup', { view: window, bubbles: true, cancelable: true, clientX: 0, clientY: 0 }))
+      })
     },
     toggleThumbsUp: null,
     toggleThumbsDown: null,
