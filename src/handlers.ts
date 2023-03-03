10af8feefe8888030c71a04b5d354a527ef06889
keifufu <github@keifufu.dev>
2023-03-03 03:55:16 +0100

    retry sending updates after event

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,35 +1,39 @@
 import { getCurrentSite, RepeatMode, SiteInfo, StateMode, WNPReduxWebSocket } from './content'
 
-export function OnMessageLegacy(self: WNPReduxWebSocket, message: string) {
+export function OnMessageLegacy(self: WNPReduxWebSocket, message: string): keyof SiteInfo | null {
   const site = getCurrentSite()
-  if (!site || !site.ready()) return
+  if (!site || !site.ready()) return null
 
+  let updateInfo: keyof SiteInfo | null = null
   try {
     const [type, data] = message.toUpperCase().split(' ')
     switch (type) {
-      case 'PLAYPAUSE': site.events.togglePlaying?.(); break
-      case 'NEXT': site.events.next?.(); break
-      case 'PREVIOUS': site.events.previous?.(); break
+      case 'PLAYPAUSE': site.events.togglePlaying?.(); updateInfo = 'state'; break
+      case 'NEXT': site.events.next?.(); updateInfo = 'title'; break
+      case 'PREVIOUS': site.events.previous?.(); updateInfo = 'title'; break
       case 'SETPOSITION': {
         // Example string: SetPosition 34:SetProgress 0,100890207715134:
         const [positionInSeconds, positionPercentageStr] = data.split(':')
         const positionPercentage = positionPercentageStr.split('SETPROGRESS ')[1]
         site.events.setPositionSeconds?.(parseInt(positionInSeconds))
         site.events.setPositionPercentage?.(parseFloat(positionPercentage.replace(',', '.')))
+        updateInfo = 'position'
         break
       }
-      case 'SETVOLUME': site.events.setVolume?.(parseInt(data)); break
-      case 'REPEAT': site.events.toggleRepeat?.(); break
-      case 'SHUFFLE': site.events.toggleShuffle?.(); break
-      case 'TOGGLETHUMBSUP': site.events.toggleThumbsUp?.(); break
-      case 'TOGGLETHUMBSDOWN': site.events.toggleThumbsDown?.(); break
-      case 'RATING': site.events.setRating?.(parseInt(data)); break
+      case 'SETVOLUME': site.events.setVolume?.(parseInt(data)); updateInfo = 'volume'; break
+      case 'REPEAT': site.events.toggleRepeat?.(); updateInfo = 'repeat'; break
+      case 'SHUFFLE': site.events.toggleShuffle?.(); updateInfo = 'shuffle'; break
+      case 'TOGGLETHUMBSUP': site.events.toggleThumbsUp?.(); updateInfo = 'rating'; break
+      case 'TOGGLETHUMBSDOWN': site.events.toggleThumbsDown?.(); updateInfo = 'rating'; break
+      case 'RATING': site.events.setRating?.(parseInt(data)); updateInfo = 'rating'; break
       default: break
     }
   } catch (e) {
     self.send(`Error:Error sending event to ${site.info.player()}`)
     self.send(`ErrorD:${e}`)
   }
+
+  return updateInfo
 }
 
 export function SendUpdateLegacy(self: WNPReduxWebSocket) {
@@ -70,7 +74,7 @@ export function SendUpdateLegacy(self: WNPReduxWebSocket) {
   })
 }
 
-export function OnMessageRev1(self: WNPReduxWebSocket, message: string) {
+export function OnMessageRev1(self: WNPReduxWebSocket, message: string): keyof SiteInfo | null {
   enum Events {
     TOGGLE_PLAYING,
     NEXT,
@@ -85,32 +89,36 @@ export function OnMessageRev1(self: WNPReduxWebSocket, message: string) {
   }
 
   const site = getCurrentSite()
-  if (!site || !site.ready()) return
+  if (!site || !site.ready()) return null
   const [type, data] = message.split(' ')
 
+  let updateInfo: keyof SiteInfo | null = null
   try {
     switch (Events[type as keyof typeof Events]) {
-      case Events.TOGGLE_PLAYING: site.events.togglePlaying?.(); break
-      case Events.NEXT: site.events.next?.(); break
-      case Events.PREVIOUS: site.events.previous?.(); break
+      case Events.TOGGLE_PLAYING: site.events.togglePlaying?.(); updateInfo = 'state'; break
+      case Events.NEXT: site.events.next?.(); updateInfo = 'title'; break
+      case Events.PREVIOUS: site.events.previous?.(); updateInfo = 'title'; break
       case Events.SET_POSITION: {
         const [positionInSeconds, positionPercentage] = data.split(':')
         site.events.setPositionSeconds?.(parseInt(positionInSeconds))
         site.events.setPositionPercentage?.(parseFloat(positionPercentage.replace(',', '.')))
+        updateInfo = 'position'
         break
       }
-      case Events.SET_VOLUME: site.events.setVolume?.(parseInt(data)); break
-      case Events.TOGGLE_REPEAT: site.events.toggleRepeat?.(); break
-      case Events.TOGGLE_SHUFFLE: site.events.toggleShuffle?.(); break
-      case Events.TOGGLE_THUMBS_UP: site.events.toggleThumbsUp?.(); break
-      case Events.TOGGLE_THUMBS_DOWN: site.events.toggleThumbsDown?.(); break
-      case Events.SET_RATING: site.events.setRating?.(parseInt(data)); break
+      case Events.SET_VOLUME: site.events.setVolume?.(parseInt(data)); updateInfo = 'volume'; break
+      case Events.TOGGLE_REPEAT: site.events.toggleRepeat?.(); updateInfo = 'repeat'; break
+      case Events.TOGGLE_SHUFFLE: site.events.toggleShuffle?.(); updateInfo = 'shuffle'; break
+      case Events.TOGGLE_THUMBS_UP: site.events.toggleThumbsUp?.(); updateInfo = 'rating'; break
+      case Events.TOGGLE_THUMBS_DOWN: site.events.toggleThumbsDown?.(); updateInfo = 'rating'; break
+      case Events.SET_RATING: site.events.setRating?.(parseInt(data)); updateInfo = 'rating'; break
       default: break
     }
   } catch (e) {
     self.send(`ERROR Error sending event to ${site.info.player()}`)
     self.send(`ERRORDEBUG ${e}`)
   }
+
+  return updateInfo
 }
 
 export function SendUpdateRev1(self: WNPReduxWebSocket) {
