ec4a2dc50246a91dcb41f0fb226f2f569d24c117
keifufu <github@keifufu.dev>
2024-01-04 11:26:01 +0100

    update package.sh

--- package.sh
+++ package.sh
@@ -5,8 +5,10 @@ SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 cd $SCRIPT_PATH
 
 rm WebNowPlaying.zip &> /dev/null
-zip -r ./dist/chrome/chrome.zip ./dist/chrome/* > /dev/null
-zip -r ./dist/firefox/firefox.zip ./dist/firefox/* > /dev/null
 zip -r WebNowPlaying.zip * -x "dist/*" "node_modules/*" > /dev/null
+cd $SCRIPT_PATH/dist/chrome
+zip -r chrome.zip * > /dev/null
+cd $SCRIPT_PATH/dist/firefox
+zip -r firefox.zip * > /dev/null
 
 cd $RETURN_PATH
